a799878cb22a24da13fb4d6e88572542983c8b96
icedchai <70030002+icedchai@users.noreply.github.com>
2025-03-09 13:29:05 -0500

    Added stylecop and updated Exiled packages

--- OmniCommonLibrary/Customs/CustomSquad.cs
+++ OmniCommonLibrary/Customs/CustomSquad.cs
@@ -1,22 +1,28 @@
-﻿
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Exiled.API.Enums;
-using UnityEngine;
-
-namespace OmniCommonLibrary
+﻿namespace OmniCommonLibrary
 {
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Exiled.API.Enums;
+    using UnityEngine;
+
+    /// <summary>
+    /// A custom data type that contains all information needed to initiate a spawn wave.
+    /// </summary>
     public class CustomSquad
     {
+        /// <summary>
+        /// Gets or sets a value indicating whether to make a CASSIE announcement upon this unit's arrival.
+        /// </summary>
         [Description("Whether to make a CASSIE announcement")]
         public bool UseCassieAnnouncement { get; set; }
+
         [Description("Name used to refer to the squad in commands and logs")]
         public string SquadName { get; set; }
-        [Description("Respawn wave this will replace. Use NineTailedFox to get the NATO divisions (Juliet-15), and ChaosInsurgency to not.")]
+        [Description("Respawn wave this will replace. Use NtfWave to get the NATO divisions (Juliett-15), and ChaosWave to not.")]
         public SpawnableFaction SquadType { get; set; }
         [Description("Announcement CASSIE will say when the custom squad enters")]
         public string EntranceAnnouncement { get; set; }
@@ -30,11 +36,11 @@ namespace OmniCommonLibrary
         public float SchematicDestructionDelay { get; set; } = 40f;
         public float SpawnDelay { get; set; } = 0f;*/
 
-        [Description("Role type corresponding to the number in the spawn queue. ONLY PUT NUMBERS 0-9.")]
+        [Description("Role type corresponding to the letters in the spawn queue. ONLY PUT one character!!")]
         public Dictionary<char,OverallRoleType> CustomRoles { get; set; } = new Dictionary<char, OverallRoleType> {
             {'0',new OverallRoleType{RoleId=5,RoleType=RoleVersion.BaseGameRole} }
         };
-        [Description("Put a string of numbers corresponding to the custom-role lookup system above.")]
+        [Description("Put a string of numbers or letters corresponding to the custom-role lookup system above.")]
         public string SpawnQueue { get; set; } = "0123456789";
 
     }
